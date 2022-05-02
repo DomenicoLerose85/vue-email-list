495e05da2967ac52cc691a75ae44b028b0cd2fa9
DomenicoLerose85 <domenicolerose85@libero.it>
2022-05-02 16:02:08 +0200

    console_error

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const app = new Vue({
     },
     methods: {},
     created() {
-        for (i = 0, i = 10, i++) {
+        for (i = 0; i = 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((res) => {
-                    this.eMails.push(res.data.response);
+                    this.eMails.push(res.data.response)
                     console.log(res.data.response)
                 }),
         }
     },
-})
+});
