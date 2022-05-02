8093c33b66e15bd108a6ec8dd70da28ce08e0fbd
DomenicoLerose85 <domenicolerose85@libero.it>
2022-05-02 19:51:01 +0200

    error_fixed_&_end_of_excercise

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 
 const app = new Vue({
     el: '#app',
-    data: {
-        eMails: [],
-    },
-    methods: {},
-    created() {
-        for (i = 0; i = 10; i++) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((res) => {
-                    this.eMails.push(res.data.response)
-                    console.log(res.data.response)
-                }),
+    data() {
+        return {
+            eMails: []
         }
     },
+    mounted() {
+        for (i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(res => {
+                    let response = res.data.response
+                    this.eMails.push(response)
+                    console.log(this.eMails);
+                })
+                .catch(error => {
+                    console.error(error)
+                });
+        }
+    }
 });
